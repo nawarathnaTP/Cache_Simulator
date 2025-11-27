6fd9a99239a94379f4ae9487025dd1bac12df578
Praveen Nawarathna <tpn.142@gmail.com>
2025-11-27 08:46:39 +0530

    Header for dmcache is done

--- DirectMapped.h
+++ DirectMapped.h
@@ -1,9 +1,32 @@
-#include <iostream>
+#ifndef DIRECT_MAPPED_CACHE_H
+#define DIRECT_MAPPED_CACHE_H
+
 #include <cstdint>
+#include <vector>
 
 struct cacheLine {
     bool valid =  false;
     uint64_t tag = 0;
 };
+
+class DirectMappedCache {
+    public:
+        DirectMappedCache(size_t cacheSize, int bytesPerBlock);
+
+        bool access(uint64_t address);
+    
+    private:
+        std::vector<cacheLine> cache;
+        size_t cacheSize;
+        int bytesPerBlock;
+
+        uint64_t hits;
+        uint64_t misses;
+
+        uint64_t getTag(uint64_t address);
+        size_t getIndex(uint64_t address);
+};
+
+#endif
 
 
